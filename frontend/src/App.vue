f7011f68df172a768cd1962e880fdd37e937cd18
harlericho <harlericho@gmail.com>
2022-06-06 20:11:58 -0500

    Developing the frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <blog-component />
+</template>
+
+<script>
+import BlogComponent from "./components/BlogComponent.vue";
+export default {
+  name: "App",
+  components: {
+    BlogComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/BlogComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Hello</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogComponent",
+};
+</script>
